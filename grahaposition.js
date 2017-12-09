657f80666ac58814b8705ffc9d2eb4c176ea7402
thozha <34402459+thozha@users.noreply.github.com>
2017-12-10 00:00:18 +0530

    Add files via upload

---
+++ grahaposition.js
@@ -0,0 +1,74 @@
+﻿var grahaPos = [
+    {
+        "ID": "1",
+        "Txt": "சூரியன்",
+        "Rasi": "விருச்சிகம்",
+        "Position":"8",
+        "Star": "புதன்",
+        "StarValue": "5"
+    },
+    {
+        "ID": "2",
+        "Txt": "சந்திரன்",
+        "Rasi": "சிம்மம்",
+        "Position": "5",
+        "Star": "சுக்கிரன்",
+        "StarValue": "6"
+    },
+    {
+        "ID": "3",
+        "Txt": "குரு",
+        "Rasi": "துலாம்",
+        "Position": "7",
+        "Star": "குரு",
+        "StarValue": "4"
+    },
+    {
+        "ID": "4",
+        "Txt": "ராகு",
+        "Rasi": "கடகம்",
+        "Position": "4",
+        "Star": "புதன்",
+        "StarValue": "5"
+    },
+    {
+        "ID": "5",
+        "Txt": "புதன்",
+        "Rasi": "தனுசு",
+        "Position": "9",
+        "Star": "கேது",
+        "StarValue": "7"
+    },
+    {
+        "ID": "6",
+        "Txt": "சுக்கிரன்",
+        "Rasi": "விருச்சிகம்",
+        "Position": "8",
+        "Star": "சனி",
+        "StarValue": "8"
+    },
+    {
+        "ID": "7",
+        "Txt": "கேது",
+        "Rasi": "மகரம்",
+        "Position": "10",
+        "Star": "செவ்வாய்",
+        "StarValue": "9"
+    },
+    {
+        "ID": "8",
+        "Txt": "சனி",
+        "Rasi": "தனுசு",
+        "Position": "9",
+        "Star": "கேது",
+        "StarValue": "7"
+    },
+    {
+        "ID": "9",
+        "Txt": "செவ்வாய்",
+        "Rasi": "துலாம்",
+        "Position": "6",
+        "Star": "செவ்வாய்",
+        "StarValue": "9"
+    }
+];
